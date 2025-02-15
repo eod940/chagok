ccd8a360b102531937513393898c73e23653490d
eod940 <eod940@gmail.com>
2025-02-15 18:27:50 +0900

    :tada: project init!!

---
+++ src/test/java/org/babyshark/chagok/ChagokApplicationTests.java
@@ -0,0 +1,13 @@
+package org.babyshark.chagok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChagokApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
